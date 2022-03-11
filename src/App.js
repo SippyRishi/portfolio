27bcc93145844be23a99bf3c45df4e3643344caa
Rishi Sippy <rishi@pulseplaydigital.comgit config --global user.name Sippyrishi>
2022-03-11 15:40:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+  
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa'
+import Header from "./components/Header";
+import Profile from "./components/profile";
+import Social  from "./components/social";
+function App() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
+  return (
+    <VStack p={5}>
+      <Flex w="100%">
+        <Heading
+          ml="8" size="md" fontWeight='semibold' color="cyan.400">imRishi</Heading>
+
+        <Spacer></Spacer>
+        <IconButton icon={<FaLinkedin />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaInstagram />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={2} icon={<FaGithub />} isRound='true' onClick={toggleColorMode}></IconButton>
+        <IconButton ml={8} icon={isDark ? <FaSun /> : <FaMoon />} isRound='true' onClick={toggleColorMode}></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
+  );
+}
+
+export default App;
